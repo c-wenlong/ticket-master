6b5117b7fdc47761efb1f8c17ebe0460565ee085
ForAeons <w.xy020203@gmail.com>
2025-01-10 19:22:00 +0800

    feat: enforce ticket size int type

--- backend/src/schema/setting.ts
+++ backend/src/schema/setting.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const SettingSchema = z.object({
   duplicate_threshold: z.number(),
-  max_ticket_size: z.number(),
+  max_ticket_size: z.number().int(),
 });
 
 export type Setting = z.infer<typeof SettingSchema>;
