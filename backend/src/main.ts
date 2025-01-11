bee33f0b621061f366fe402ee66b5e710203f198
ForAeons <w.xy020203@gmail.com>
2025-01-11 12:59:28 +0800

    misc: update imports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
+import { loadConfig } from "@/config";
 import mongo from "@/db";
 import { setupRoutes } from "@/handler";
+import { errorMiddleware } from "@/middleware/error";
 import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 import { logger } from "hono/logger";
-import { loadConfig } from "./config";
-import { errorMiddleware } from "./middleware/error";
 
 const cfg = await loadConfig();
 await mongo.MustInit(cfg);
