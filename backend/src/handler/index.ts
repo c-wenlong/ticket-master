cfdaccb1eb25bdc4b1b89f947dcf6df4bc2d1557
ForAeons <w.xy020203@gmail.com>
2025-01-16 16:56:40 +0800

    feat: add analytics routes

--- backend/src/handler/index.ts
+++ backend/src/handler/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 
-import { metricRoutes } from "./metrics";
+import { analyticsRoutes } from "./analytics";
 
 export const setupRoutes = (app: Hono) => {
-  app.route("/metrics", metricRoutes);
+  app.route("/analytics", analyticsRoutes);
 }
