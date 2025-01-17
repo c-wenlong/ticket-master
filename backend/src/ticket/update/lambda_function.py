cdf43d9f2b7c2ee1ae89802b1f4fdb73dc36c4b1
ForAeons <w.xy020203@gmail.com>
2025-01-17 22:36:36 +0800

    fix: update vector of point as well

--- backend/src/ticket/update/lambda_function.py
+++ backend/src/ticket/update/lambda_function.py
@@ -1,13 +1,16 @@
 import json
 import os
+from openai import OpenAI
 from datetime import datetime
 from typing import Dict
 from dotenv import load_dotenv
 from qdrant_client import QdrantClient
+from qdrant_client.models import PointVectors
 from qdrant_client.http.models import PointStruct
 
 load_dotenv()
 
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 QDRANT_URL = os.environ["QDRANT_URL"]
 QDRANT_API_KEY = os.environ["QDRANT_API_KEY"]
 QDRANT_COLLECTION_NAME = os.environ["QDRANT_COLLECTION_NAME"]
@@ -16,6 +19,7 @@ qdrant_client = QdrantClient(
     url=QDRANT_URL,
     api_key=QDRANT_API_KEY,
 )
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 
 def lambda_handler(event, _context):
@@ -66,9 +70,23 @@ def select_ticket(ticket_id: int):
     return ticket[0]
 
 
+def text_to_embedding(text):
+    embeddings = openai_client.embeddings.create(
+        model="text-embedding-3-small", input=text, encoding_format="float"
+    )
+    return embeddings.data[0].embedding
+
+
 def update_ticket_payload(ticket_id: int, ticket_delta: Dict):
-    res = qdrant_client.set_payload(
+    qdrant_client.set_payload(
         collection_name=QDRANT_COLLECTION_NAME, points=[ticket_id], payload=ticket_delta
     )
 
-    return res
+    qdrant_client.update_vectors(
+        collection_name=QDRANT_COLLECTION_NAME,
+        points=[
+            PointVectors(
+                id=ticket_id, vector=text_to_embedding(ticket_delta["session_name"])
+            ),
+        ],
+    )
