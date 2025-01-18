108cc0178c4f9a956d23ba0bd1af200b58eb70ab
xiaoyunwu.w <xiaoyunwu.w@bytedance.com>
2025-01-18 15:50:37 +0800

    chore: move analytics backend to another folder

---
+++ backend/analytics/src/config.ts
@@ -0,0 +1,48 @@
+export type AppConfig = {
+  dbUserName: string;
+  dbPassword: string;
+  dbHost: string;
+  dbName: string;
+  dbPort: string;
+  devHttpPort: number;
+};
+
+export const loadConfig = async () => {
+  if (process.env.NODE_ENV !== "production") {
+    await loadDotenv();
+  }
+
+  const config: AppConfig = {
+    dbUserName: process.env.DOCUMENTDB_USERNAME ?? "admin",
+    dbPassword: process.env.DOCUMENTDB_PASSWORD ?? "password",
+    dbHost: process.env.DOCUMENTDB_HOSTNAME ?? "localhost",
+    dbName: process.env.DOCUMENTDB_DATABASE ?? "ticket_master",
+    dbPort: process.env.PORT ?? "27018",
+    devHttpPort: Number(process.env.DEV_HTTP_PORT ?? "3000"),
+  };
+
+  return config;
+};
+
+const loadDotenv = async () => {
+  const path = await import("path");
+  const fs = await import("fs");
+  const dotenv = await import("dotenv");
+
+  const dotenvPath = path.resolve(__dirname, "../../../.env");
+
+  if (!fs.existsSync(dotenvPath)) {
+    console.warn("No .env file found in root of project directory, using default values");
+    return;
+  }
+
+  const out = dotenv.config({
+    path: dotenvPath,
+  });
+
+  if (out.error) {
+    throw out.error;
+  }
+
+  console.log("Loaded .env file in development");
+};
---
+++ backend/analytics/src/db/index.ts
@@ -0,0 +1,47 @@
+import { Db, MongoClient } from "mongodb";
+
+import { AppConfig } from "@/config";
+
+let mongoClient: Optional<MongoClient> = null;
+let db: Optional<Db> = null;
+
+const MustInit = async (cfg: AppConfig) => {
+  let uri = `mongodb://${cfg.dbUserName}:${encodeURIComponent(
+    cfg.dbPassword,
+  )}@${cfg.dbHost}:${
+    cfg.dbPort
+  }/?readPreference=secondaryPreferred&retryWrites=false`;
+  if (process.env.NODE_ENV === "production") {
+    uri += "&tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0";
+  }
+
+  mongoClient = new MongoClient(uri);
+  await mongoClient.connect();
+  db = mongoClient.db(cfg.dbName);
+
+  console.info("MongoDB connected");
+};
+
+export default {
+  get client() {
+    if (!mongoClient) {
+      throw new Error("MongoDB client is not initialized");
+    }
+
+    return mongoClient;
+  },
+
+  get db() {
+    if (!db) {
+      throw new Error("MongoDB database is not initialized");
+    }
+
+    return db;
+  },
+
+  MustInit,
+};
+
+export * from "./constant";
+export * from "./type";
+
