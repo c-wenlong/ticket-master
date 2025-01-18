e9309bfbeaa0cd32ed2f7b5ba52096a9148b5901
avellinwong01 <avellin.wong@gmail.com>
2025-01-18 22:40:54 +0800

    Add charts to render analytics on streamlit frontend

--- frontend/src/utils/__init__.py
+++ frontend/src/utils/__init__.py
@@ -1,8 +1,9 @@
 from .file_manager import read_json
-from .ticket_operations import get_sample_tickets, unpack_ticket
+from .ticket_operations import get_sample_tickets, unpack_ticket, get_sample_analytics
 
 __all__ = [
     "read_json",
     "get_sample_tickets",
     "unpack_ticket",
+    "get_sample_analytics",
 ]
